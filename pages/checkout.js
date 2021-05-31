c0688d5bac70ea1bab999cad04a800ea638b3c93
maxMaksum <maxmaksum@gmail.com>
2021-05-31 06:41:43 -0700

    never give up

--- pages/checkout.js
+++ pages/checkout.js
@@ -10,7 +10,8 @@ import {useState} from 'react'
 import {  loadStripe } from '@stripe/stripe-js';
 
 
-let stripePromise = loadStripe(`${process.env.stripe_public_key}`)
+let stripePromise = loadStripe(`${process.env.
+  NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
 
 
 function checkout() {
